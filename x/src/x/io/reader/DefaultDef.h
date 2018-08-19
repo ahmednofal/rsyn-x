f68bbbfecf05d3a1c53c4dc128c4380e46e30fb3
kareefardi <kareemfarid@aucegypt.edu>
2018-08-20 01:28:17 +0200

    doxygen docs attempt

--- x/src/x/io/reader/DefaultDef.h
+++ x/src/x/io/reader/DefaultDef.h
@@ -5,10 +5,22 @@
 #include <Rsyn/PhysicalDesign>
 #include <Rsyn/Session>
 
+
+//! Creates A Minimal Def Dscp
+/*!
+ * Creates A Def Dscp out of
+ * the current design built in rsyn.
+ *
+ * It fills components, Ports, Nets and
+ * Database units
+ */
+
 class DefaultDef {
 
 public:
+    //! Default constructor
     DefaultDef();
+    //! Returns the created def dscp
     DefDscp getDscp();
 
 private:
--- x/src/x/io/reader/NetlistReader.h
+++ x/src/x/io/reader/NetlistReader.h
@@ -9,14 +9,26 @@
 
 namespace Rsyn {
 
+//! A Reader Class
+/*!
+ * A Reader class that accepts a 
+ * verilog netlist and a lef
+ * and inputs the design to rsyn
+ */
+
 class NetlistReader : public Reader {
 public:
     NetlistReader() = default;
     virtual ~NetlistReader() = default;
     virtual bool load(const Rsyn::Json &params) override;
 
+    //! Verilog path setter
     void setNetlistPath(std::string);
+
+    //! Lef path setter
     void setLefPath(std::string);
+
+    //! Loads designs into rsyn
     void loadAll();
 
 private:
@@ -26,8 +38,17 @@ private:
     Rsyn::Design mDesign;
 	LefDscp lefDscp;
     
+    //! Loads Verilog into rsyn
     void loadDesign();
+
+    //! Loads lef into rsyn
     void loadLib();
+
+    //! Loads some def defaults into rsyn
+    /*!
+     * Some internal data structure depends on initilizing
+     * rsyn design with a def descpritor
+     */
     void loadDefaults();
 };
 
